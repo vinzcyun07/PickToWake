d7dc01318241f32b8ecb0ddf77cb7e15391b80af
vinzcyun07 <vinzcyun@proton.me>
2026-02-01 01:50:03 -0800

    Add BootReceiver.kt

---
+++ app/src/main/java/com/wyn/ptw/BootReceiver.kt
@@ -0,0 +1,15 @@
+package com.wyn.ptw
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            if (SettingsStore.isEnabled(context)) {
+                WakeService.start(context)
+            }
+        }
+    }
+}
